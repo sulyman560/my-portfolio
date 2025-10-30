8c09e9fae49d362dcdeffad993f44f79fbc56fd0
sulyman560 <sulyman560@gmail.com>
2025-10-30 17:58:40 +0600

    fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -42,7 +42,6 @@ const Navbar = ({ isDarkMode, setIsDarkMode }) => {
           <li><a className='font-Ovo' href="#services">Services</a></li>
           <li><a className='font-Ovo' href="#work">My Work</a></li>
           <li><a className='font-Ovo' href="#project">Project</a></li>
-          <li><a className='font-Ovo' href="#projects">Projects</a></li>
           <li><a className='font-Ovo' href="#contact">Contact me</a></li>
         </ul>
 
