989a0fd20698fc56c747e4b80151a49e4451ea8e
sulyman560 <sulyman560@gmail.com>
2025-10-31 22:47:33 +0600

    fix

--- app/page.js
+++ app/page.js
@@ -4,7 +4,6 @@ import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
-import Project from "@/components/Project";
 import Projects from "@/components/Projects";
 import Services from "@/components/Services";
 import Work from "@/components/Work";
--- components/Header.jsx
+++ components/Header.jsx
@@ -36,21 +36,22 @@ const Header = () => {
         I create modern and responsive websites that help businesses establish a strong online presence and engage their target audience effectively.
       </motion.p>
       <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
-        <motion.a 
-        initial={{ y: 30, opacity: 0 }}
-        whileInView={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.6, delay: 1 }}
-        href="#contact" className='px-10 py-3 border border-white rounded-full bg-black/90 text-white flex 
-          items-center gap-2 hover:bg-black transition dark:bg-transparent'>Contact me <Image src={assets.right_arrow_white}
+        <motion.a
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+          href="#contact" className='px-10 py-3 border border-white rounded-full bg-black/90 text-white flex 
+          items-center gap-2 hover:bg-black hover:bg-black transition duration-500 dark:bg-transparent'>
+          Contact me <Image src={assets.right_arrow_white}
             className='w-4' alt='' /></motion.a>
 
-        <motion.a 
-        initial={{ y: 30, opacity: 0 }}
-        whileInView={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.6, delay: 1.2 }}
-        href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex 
+        <motion.a
+          initial={{ y: 30, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 1.2 }}
+          href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex 
           items-center gap-2 transition dark:bg-white dark:text-black'>my resume <Image src={assets.download_icon} className='w-4' alt='' />
-          </motion.a>
+        </motion.a>
       </div>
     </div>
   )
